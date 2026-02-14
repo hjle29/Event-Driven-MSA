066063eeac37c7d9c4a761944dd75c78c237827c
HJLE <02q09q@gmail.com>
2026-02-15 01:59:28 +0900

    feat: add common Response

---
+++ common/src/main/java/com/hjle/common/dto/response/ApiResponse.java
@@ -0,0 +1,28 @@
+package com.hjle.common.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ApiResponse<T> {
+    private String status;
+    private String message;
+    private T data;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return ApiResponse.<T>builder()
+                .status("success")
+                .message(null)
+                .data(data)
+                .build();
+    }
+
+    public static ApiResponse<Void> error(String message) {
+        return ApiResponse.<Void>builder()
+                .status("error")
+                .message(message)
+                .data(null)
+                .build();
+    }
+}
